363ba604be197218a677fe0fe4f6b8ed11fe05ac
RaquelNayyara <quelaazzz@gmail.com>
2023-09-27 15:45:57 +0700

    tutor 4 beres

--- main/urls.py
+++ main/urls.py
@@ -3,6 +3,8 @@ from main.views import show_main, create_product, show_xml, show_json, show_xml_
 from main.views import register #sesuaikan dengan nama fungsi yang dibuat
 from main.views import login_user #sesuaikan dengan nama fungsi yang dibuat
 from main.views import logout_user
+from main.views import edit_product
+from main.views import delete_product
 
 app_name = 'main'
 
@@ -16,4 +18,6 @@ urlpatterns = [
     path('register/', register, name='register'), #sesuaikan dengan nama fungsi yang dibuat
     path('login/', login_user, name='login'), #sesuaikan dengan nama fungsi yang dibuat
     path('logout/', logout_user, name='logout'),
+    path('edit-product/<int:id>', edit_product, name='edit_product'),
+    path('delete/<int:id>', delete_product, name='delete_product'), # sesuaikan dengan nama fungsi yang dibuat
 ]
